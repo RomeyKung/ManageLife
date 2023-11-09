552d65eac438d30eff007ac5abd71f630a0432aa
RomeyKung <88416990+RomeyKung@users.noreply.github.com>
2023-11-09 14:36:56 +0700

    Update AuthenStack.js

--- FrontEnd/Team/Auth/AuthenStack.js
+++ FrontEnd/Team/Auth/AuthenStack.js
@@ -1,20 +1,40 @@
-import React, { useEffect } from 'react'
-import { View, Text } from 'react-native'
-import { createStackNavigator } from '@react-navigation/stack';
-import Login from './Pages/Login';
-import Signup from './Pages/Signup';
-import Drawer from './Drawer';
+import React, { useEffect } from "react";
+import { View, Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import Drawer from "./Drawer";
+import FIREBASE_AUTH from "../../FirebaseConfig";
 const AuthenStack = () => {
-    const Stack = createStackNavigator();
+  const Stack = createStackNavigator();
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const unsubscribe = FIREBASE_AUTH.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+    return unsubscribe;
+  }, []);
 
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name="Login" component={Login} />
-            <Stack.Screen name="Signup" component={Signup} />
-            <Stack.Screen name="Drawer" component={Drawer} options={{headerShown: false}}/>
-        </Stack.Navigator>
-    )
-}
+  return (
+    <Stack.Navigator>
+      {user ? (
+        <>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Signup" component={Signup} />
+        </>
+      ) : (
+        <Stack.Screen
+          name="Drawer"
+          component={Drawer}
+          options={{ headerShown: false }}
+        />
+      )}
+    </Stack.Navigator>
+  );
+};
 
-export default AuthenStack
-
+export default AuthenStack;
