3e8ab0564e0111bcdf3bdfe7bd99fc1917651e41
RomeyKung <88416990+RomeyKung@users.noreply.github.com>
2023-11-10 21:59:24 +0700

    fix something + react paper

--- FrontEnd/Team/Auth/Drawer.js
+++ FrontEnd/Team/Auth/Drawer.js
@@ -1,53 +1,71 @@
-import { StyleSheet, Image, Text, View, TouchableOpacity } from 'react-native'
-import { HeaderTitle } from '@react-navigation/elements';
-import React from 'react'
-import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
-import SchedulerStack from '../Scheduler/Navigations/SchedulerStack';
-import TodoListStack from '../TodoList/Navigations/TodoListStack';
-import HealthStack from '../Health/Navigations/HealthStack';
-import NoteStack from '../Notes/Navigations/NoteStack';
-import UserSettings from '../User/Pages/UserSetting';
+import { StyleSheet, Image, Text, View, TouchableOpacity } from "react-native";
+import { HeaderTitle } from "@react-navigation/elements";
+import React from "react";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
+import SchedulerStack from "../Scheduler/Navigations/SchedulerStack";
+import TodoListStack from "../TodoList/Navigations/TodoListStack";
+import HealthStack from "../Health/Navigations/HealthStack";
+import NoteStack from "../Notes/Navigations/NoteStack";
+import UserSettings from "../User/Pages/UserSetting";
+import InventoryStack from "../Inventory/Navigations/InventoryStack";
 import { getAuth } from "firebase/auth";
-import { useDispatch, useSelector } from 'react-redux';
-import { FontAwesome, AntDesign, Ionicons, Octicons } from '@expo/vector-icons';
-import { FIREBASE_AUTH } from '../../FirebaseConfig';
-import axios from 'axios';
-import LocalIP from '../LocalIP';
-import { saveUserData } from '../../redux/userSlice';
+import { useDispatch, useSelector } from "react-redux";
+import { FontAwesome, AntDesign, Ionicons, Octicons } from "@expo/vector-icons";
+import { FIREBASE_AUTH } from "../../FirebaseConfig";
+import axios from "axios";
+import LocalIP from "../LocalIP";
+import { saveUserData } from "../../redux/userSlice";
 
-const dispatch = useDispatch()
 const CustomDrawerContent = (props) => {
-  const auth = getAuth()
-  axios.get(`http://${LocalIP}:8082/user-service/user/${auth.currentUser.uid}`).then(res => dispatch(saveUserData(res.data))).catch(er => console.log(er))
+  const dispatch = useDispatch();
+  const auth = getAuth();
+  // axios
+  //   .get(`http://${LocalIP}:8082/user-service/user/${auth.currentUser.uid}`)
+  //   .then((res) => dispatch(saveUserData(res.data)))
+  //   .catch((er) => console.log(er));
 
-  
   return (
     <View style={{ flex: 1 }}>
       <View style={{ flex: 9 }}>
-        <DrawerContentScrollView  {...props}>
-          {user ? <View style={{ padding: 15 }}>
-            <Image style={{ width: 100, height: 100, borderRadius: 50 }} source={user.userImage ? { uri: user.userImage } : require("../../assets/icon.png")} />
-            <Text style={{ fontSize: 32, fontWeight: 'bold' }}>{user.username}</Text>
-          </View>:null}
+        <DrawerContentScrollView {...props}>
+          {/* {user ? (
+            <View style={{ padding: 15 }}>
+              <Image
+                style={{ width: 100, height: 100, borderRadius: 50 }}
+                source={
+                  user.userImage
+                    ? { uri: user.userImage }
+                    : require("../../assets/icon.png")
+                }
+              />
+              <Text style={{ fontSize: 32, fontWeight: "bold" }}>
+                {user.username}
+              </Text>
+            </View>
+          ) : null} */}
 
           <DrawerItemList {...props} />
         </DrawerContentScrollView>
       </View>
       <View style={{ borderTopColor: "#ccc", borderTopWidth: 1 }}>
-        <TouchableOpacity onPress={() => {
-          FIREBASE_AUTH.signOut()
-        }} style={{ paddingVertical: 20, marginLeft: 20 }}>
-
+        <TouchableOpacity
+          onPress={() => {
+            FIREBASE_AUTH.signOut();
+          }}
+          style={{ paddingVertical: 20, marginLeft: 20 }}
+        >
           <View style={{ alignItems: "center", flexDirection: "row" }}>
             <FontAwesome name="sign-out" size={24} color="black" />
-            <Text>  Sign Out</Text>
+            <Text> Sign Out</Text>
           </View>
         </TouchableOpacity>
       </View>
-
-
-
-    </View >
+    </View>
   );
 };
 const Drawer = () => {
@@ -57,68 +75,98 @@ const Drawer = () => {
       initialRouteName="SchedulerStack"
       screenOptions={{
         headerStyle: {
-          backgroundColor: '#88CF88',
+          backgroundColor: "#88CF88",
         },
-        headerTintColor: 'black',
+        headerTintColor: "black",
         headerTitleStyle: {
-          display: 'none', // Hide the default title
-        }
+          display: "none", // Hide the default title
+        },
       }}
       drawerContent={(props) => <CustomDrawerContent {...props} />}
     >
-      <Drawer.Screen options={{
-        headerStyle: {
-          backgroundColor: '#88CF88',
-        },
-        headerTintColor: 'black',
-        drawerIcon: ({ color }) => (
-          <AntDesign name="calendar" size={24} color={color} />
-        )
-      }} name="SchedulerStack" component={SchedulerStack} />
-      <Drawer.Screen options={{
-        headerStyle: {
-          backgroundColor: '#88CF88',
-        },
-        headerTintColor: 'black',
-        drawerIcon: ({ color }) => (
-          <Octicons name="checklist" size={24} color={color} />
-        )
-      }} name="TodoListStack" component={TodoListStack} />
-      <Drawer.Screen name="HealthStack" component={HealthStack} options={{
-        headerStyle: {
-          backgroundColor: '#88CF88',
-        },
-        headerTintColor: 'black',
-        drawerIcon: ({ color }) => (
-          <Ionicons name="fitness" size={24} color={color} />
-        )
-      }}
+      <Drawer.Screen
+        options={{
+          headerStyle: {
+            backgroundColor: "#88CF88",
+          },
+          headerTintColor: "black",
+          drawerIcon: ({ color }) => (
+            <AntDesign name="calendar" size={24} color={color} />
+          ),
+        }}
+        name="SchedulerStack"
+        component={SchedulerStack}
       />
-      <Drawer.Screen name="NoteStack" component={NoteStack} options={{
-        headerStyle: {
-          backgroundColor: '#88CF88',
-        },
-        headerTintColor: 'black',
-        drawerIcon: ({ color }) => (
-          <FontAwesome name="sticky-note" size={24} color={color} />
-        )
-      }}
+      <Drawer.Screen
+        options={{
+          headerStyle: {
+            backgroundColor: "#88CF88",
+          },
+          headerTintColor: "black",
+          drawerIcon: ({ color }) => (
+            <Octicons name="checklist" size={24} color={color} />
+          ),
+        }}
+        name="TodoListStack"
+        component={TodoListStack}
       />
-      <Drawer.Screen name="UserSettings" initialParams={user} component={UserSettings} options={{
-        headerStyle: {
-          backgroundColor: '#88CF88',
-        },
-        headerTintColor: 'black',
-        drawerIcon: ({ color }) => (
-          <Ionicons name="person" size={24} color={color} />
-        )
-      }}
+      <Drawer.Screen
+        name="HealthStack"
+        component={HealthStack}
+        options={{
+          headerStyle: {
+            backgroundColor: "#88CF88",
+          },
+          headerTintColor: "black",
+          drawerIcon: ({ color }) => (
+            <Ionicons name="fitness" size={24} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="NoteStack"
+        component={NoteStack}
+        options={{
+          headerStyle: {
+            backgroundColor: "#88CF88",
+          },
+          headerTintColor: "black",
+          drawerIcon: ({ color }) => (
+            <FontAwesome name="sticky-note" size={24} color={color} />
+          ),
+        }}
+      />
+      {/* <Drawer.Screen
+        name="UserSettings"
+        initialParams={user}
+        component={UserSettings}
+        options={{
+          headerStyle: {
+            backgroundColor: "#88CF88",
+          },
+          headerTintColor: "black",
+          drawerIcon: ({ color }) => (
+            <Ionicons name="person" size={24} color={color} />
+          ),
+        }}
+      /> */}
+      <Drawer.Screen
+        name="InventoryStack"
+        component={InventoryStack}
+        options={{
+          headerStyle: {
+            backgroundColor: "#88CF88",
+          },
+          headerTintColor: "black",
+          drawerIcon: ({ color }) => (
+            <Ionicons name="person" size={24} color={color} />
+          ),
+        }}
       />
     </Drawer.Navigator>
-  )
-}
+  );
+};
 
+export default Drawer;
 
-export default Drawer
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
