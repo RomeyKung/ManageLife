ba50b22b8e4fcf61bdf2457df6858c8079a5620b
Kampanat Konklong <64070125@it.kmitl.ac.th>
2025-03-24 16:53:16 +0700

    Update FirebaseConfig.js

--- FrontEnd/FirebaseConfig.js
+++ FrontEnd/FirebaseConfig.js
@@ -6,12 +6,12 @@ import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 // import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 // ปรับปรุงการกำหนดค่า Firebase ของเว็บแอปของคุณ
 const firebaseConfig = {
-  apiKey: "AIzaSyCJxCDI2R4Gys96m0FRb2NZNcQkXc0jt7E",
-  authDomain: "managelife-74367.firebaseapp.com",
-  projectId: "managelife-74367",
-  storageBucket: "managelife-74367.appspot.com",
-  messagingSenderId: "34544116724",
-  appId: "1:34544116724:web:7de2483f9a783230c10fa1",
+  apiKey: "qwe",
+  authDomain: "qwe",
+  projectId: "qwe",
+  storageBucket: "qwe",
+  messagingSenderId: "qwe",
+  appId: "qwe",
 };
 
 // ตั้งค่า Firebase
